f948cda2af7f03e797fee0f0a1c063ba316f0ce8
rootaccess2023 <134943577+rootaccess2023@users.noreply.github.com>
2023-05-31 22:59:40 +0800

    Create Customer_Analysis.sql

---
+++ Customer_Analysis.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM Customer
+WHERE FirstName LIKE 'a%';
+
+SELECT *
+FROM Customer 
+WHERE FirstName LIKE '%a';
+
+SELECT *
+FROM Customer 
+WHERE FirstName LIKE '%a%';
+
+SELECT *
+FROM Artist
+WHERE Name LIKE 'Aerosmith';
